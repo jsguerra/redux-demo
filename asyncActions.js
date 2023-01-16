10a62d6f77725626e3cb9780ea81f27c6e291f19
jsguerra <jose@gcostudios.com>
2023-01-16 16:38:56 +0000

    setup async example

---
+++ asyncActions.js
@@ -0,0 +1,61 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
+// Initial state
+const initialState = {
+  loading: false,
+  users: [],
+  error: "",
+};
+
+// Actions
+const FETCH_USERS_REQUESTED = "FETCH_USERS_REQUESTED";
+const FETCH_USERS_SUCCEEDED = "FETCH_USERS_SUCCEEDED";
+const FETCH_USERS_FAILED = "FETCH_USERS_FAILED";
+
+// Action creators
+const fetchUsersRequst = () => {
+  return {
+    type: FETCH_USERS_REQUESTED,
+  };
+};
+
+const fetchUsersSuccess = (users) => {
+  return {
+    type: FETCH_USERS_SUCCEEDED,
+    payload: users,
+  };
+};
+
+const fetchUsersFailure = (error) => {
+  return {
+    type: FETCH_USERS_FAILED,
+    payload: error,
+  };
+};
+
+// Reducer function
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USERS_REQUESTED:
+      return {
+        ...state,
+        loading: true,
+      };
+    case FETCH_USERS_SUCCEEDED:
+      return {
+        loading: false,
+        users: action.payload,
+        error: "",
+      };
+    case FETCH_USERS_FAILED:
+      return {
+        loading: false,
+        users: [],
+        error: action.payload,
+      };
+  }
+};
+
+// Create Store
+const store = createStore(reducer);
